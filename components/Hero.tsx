ea943e5498fd04e7678667672ee99846e2629d7a
Jahfar Ali <evjahfar@gmail.com>
2024-08-02 23:01:20 +0530

    home-page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import Particle from './Particle'
+import React from "react";
+import TextEffect from "./TextEffect";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="h-[88vh] vg-[url('/images/banner.jpg')] bg-cover bg-center">
-      <Particle/>
+    <div className="h-[88vh] bg-[url('/images/banner.jpg')] bg-cover bg-center">
+      <div className="w-[80%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-[3rem] h-full items-center">
+        <div>
+          <h1 className="text-[35px] md:text-[50px] text-white font-bold">
+            HI, I'M <span className="text-yellow-400">JAHFAR!</span>
+          </h1>
+          <TextEffect />
+        </div>
+        <div className="relative w-[500px] h-[500px] bg-[#55e6a5] rounded-full hidden lg:flex items-center justify-center">
+          <Image
+            src="/user.jpeg"
+            alt="user"
+            layout="fill"
+            objectFit="cover"
+            className="rounded-full"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,51 @@
+// import Hero from "@/components/Hero";
+// import MobileNav from "@/components/MobileNav";
+// import Nav from "@/components/Nav";
+// import React, { useState } from "react";
+
+// const HomePage = () => {
+//   const [nav, setNav] = useState(false);
+//   const openNav = () => setNav(true);
+//   const closeNav = () => setNav(false);
+//   return (
+//     <div className="overflow-x-hidden">
+//       <div>
+//         {/*Navbar section */}
+//         <MobileNav nav={nav} closeNav={closeNav} />
+//         <Nav openNav={openNav} />
+//         {/*Hero section */}
+//         <Hero/>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default HomePage;
+
 import Hero from "@/components/Hero";
 import MobileNav from "@/components/MobileNav";
 import Nav from "@/components/Nav";
 import React, { useState } from "react";
+import Particle from "@/components/Particle"; // Make sure the import path is correct
 
 const HomePage = () => {
   const [nav, setNav] = useState(false);
   const openNav = () => setNav(true);
   const closeNav = () => setNav(false);
+  
   return (
-    <div className="overflow-x-hidden">
-      <div>
-        {/*Navbar section */}
+    <div className="relative min-h-screen overflow-x-hidden">
+      <Particle /> {/* Add the Particle component for the background effect */}
+      <div className="absolute top-0 left-0 right-0 z-10">
+        {/* Navbar section */}
         <MobileNav nav={nav} closeNav={closeNav} />
         <Nav openNav={openNav} />
-        {/*Hero section */}
-        <Hero/>
+        {/* Hero section */}
+        <Hero />
       </div>
     </div>
   );
 };
 
 export default HomePage;
+
