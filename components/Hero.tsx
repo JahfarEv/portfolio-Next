5095ec30282671cdb9dde5878e890103520b508f
Jahfar Ali <evjahfar@gmail.com>
2024-08-11 09:30:40 +0530

    deploy

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,10 @@ const Hero = () => {
       <div className="w-[80%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-[3rem] h-full items-center">
         <div>
           <h1 className="text-[35px] md:text-[50px] text-white font-bold">
-            HI, I'M <span className="bg-gradient-to-r from-teal-400 to-cyan-500 bg-clip-text text-transparent">JAHFAR ALI!</span>
+            HI, I&#39;M{" "}
+            <span className="bg-gradient-to-r from-teal-400 to-cyan-500 bg-clip-text text-transparent">
+              JAHFAR ALI!
+            </span>
           </h1>
 
           <TextEffect />
@@ -20,11 +23,14 @@ const Hero = () => {
           <div className="mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6">
             <button className="px-[2rem] hover:bg-teal-500 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-white text-black flex items-center space-x-2">
               <p>Download cv</p>
-              <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black"/>
-              </button>
+              <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
+            </button>
           </div>
         </div>
-        <div data-aos='zoom-in' className="relative w-[450px] h-[450px] rounded-full hidden lg:flex items-center justify-center mt-[15vh]">
+        <div
+          data-aos="zoom-in"
+          className="relative w-[450px] h-[450px] rounded-full hidden lg:flex items-center justify-center mt-[15vh]"
+        >
           <Image
             src="/profile2.png"
             alt="user"
