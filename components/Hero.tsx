7ea734ccbb708278f0939a8a8859c74c01928b6c
Jahfar Ali <evjahfar@gmail.com>
2024-08-28 11:32:11 +0530

    updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
       <div className="w-[80%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-[3rem] h-full items-center">
         <div>
           <h1 className="text-[35px] md:text-[50px] text-white font-bold">
-            HI, I&apos;M
+            HI, IAM
             <span className="bg-gradient-to-r from-teal-400 to-cyan-500 bg-clip-text text-transparent">
               JAHFAR ALI!
             </span>
@@ -18,8 +18,9 @@ const Hero = () => {
 
           <TextEffect />
           <p className="mt-[1.5rem] text-[20px] text-[#ffffff92]">
-          Hi! I’m Jahfar Ali, a dedicated MERN Stack Developer with a knack for crafting seamless and responsive web applications. Explore my work, dive into my projects, and see how I turn ideas into functional digital experiences. Let's build something amazing together!
-          </p>
+  Hi! I am Jahfar Ali, a dedicated MERN Stack Developer with a knack for crafting seamless and responsive web applications. Explore my work, dive into my projects, and see how I turn ideas into functional digital experiences. Let&#39;s build something amazing together!
+</p>
+
           <div className="mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6">
            <Link href="https://drive.google.com/file/d/15x8WHLoxMKJrpN7td5bl1xReAHnhm04n/view?usp=sharing">
             <button className="px-[2rem] hover:bg-teal-500 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-white text-black flex items-center space-x-2">
